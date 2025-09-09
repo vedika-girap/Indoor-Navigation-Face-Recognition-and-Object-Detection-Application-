3b61a9acfdfc679c527c87176c6c59b488eb41d0
Sameeksha <sameekshasameeksham@gmail.com>
2025-09-09 22:51:08 +0530

    Update text_to_speech.py

--- backend/VoiceAssistantOnline/services/text_to_speech.py
+++ backend/VoiceAssistantOnline/services/text_to_speech.py
@@ -1,9 +1,7 @@
 import pyttsx3
 from utils.logger import log
-
-engine = pyttsx3.init()
-
+engine=pyttsx3.init()
 def speak(text):
-    log(f"🗣️ Speaking: {text}", "info")
+    log(f"Speaking: {text}", "info")
     engine.say(text)
     engine.runAndWait()
