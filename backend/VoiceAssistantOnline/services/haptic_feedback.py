9a75aa48bcfbd7e4d2266563d61bba88200e0c32
Sameeksha <sameekshasameeksham@gmail.com>
2025-09-09 22:49:15 +0530

    Update haptic_feedback.py

--- backend/VoiceAssistantOnline/services/haptic_feedback.py
+++ backend/VoiceAssistantOnline/services/haptic_feedback.py
@@ -1,19 +1,15 @@
 from utils.logger import log
 import time
-
 def trigger_haptic(intent):
-    patterns = {
+    patterns={
         "locate_library": [0.2, 0.2, 0.2],
         "locate_washroom": [0.5, 0.5],
         "locate_classroom": [1.0]
     }
-
     pattern = patterns.get(intent)
     if not pattern:
         return
-
-    log(f"🔔 Triggering haptic feedback for {intent}", "info")
-
+    log(f"Triggering haptic feedback for {intent}", "info")
     for p in pattern:
-        log("💥 Vibration", "info")
+        log("Vibration", "info")
         time.sleep(p)
